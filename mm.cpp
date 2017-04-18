b7b8cd31563efd3c707b12df528a5559a09a6c7a
Jakub Stejskal <xstejs24@stud.fit.vutbr.cz>
2017-04-18 19:24:16 +0200

    Algoritmus funkční

--- mm.cpp
+++ mm.cpp
@@ -24,6 +24,8 @@ MPI_Status stat;
 
 #define BUFF_SIZE 1
 #define TAG 0
+#define TAG_A 1
+#define TAG_B 2
 
 typedef struct{
 	int processCount;
@@ -31,6 +33,7 @@ typedef struct{
 	int rows;
 	int cols;
 	int C;
+	int matN;
 } T_PROCESOR;
 
 typedef struct{
@@ -77,17 +80,15 @@ int loadInput(T_PROCESOR *mat, T_MATRIX *matrix, ifstream *file){
 	stringstream ss(input);
 	// Plnění alokované matice
 	while(getline(ss,line,'\n')){
-		cerr<<line<<endl;
+//		cerr<<line<<endl;
 		stringstream iss(line);
 		while(iss >> number){
-			cerr<<"Zpracovávám: "<<number<<endl;
 			if(!isNumber(number))
 			{
 				cerr<<"Špatný formát vstupní matice! Matice obsahuje nepodporované znaky!"<<endl;
 				exit(EXIT_FAILURE);
 			}
 			matrix->matrix[tmpR][tmpC] = atoi(number.c_str());
-			cerr<<"Vloženo: "<<matrix->matrix[tmpR][tmpC]<<endl;
 //			cerr<<"TestX: "<<matrix[tmpR][tmpC]<<endl;
 			tmpC++;
 		}
@@ -132,6 +133,8 @@ int main(int argc, char *argv[])
 	
 	ifstream mat1("mat1");
 	ifstream mat2("mat2");
+	
+	matrix->matN = atoi(argv[1]);
 
     string fLine;
     getline(mat1, fLine);
@@ -142,6 +145,8 @@ int main(int argc, char *argv[])
 	if(matrix->procID == 0){
 		int tmpR;
 		int tmpC;
+		int valA;
+		int valB;
 		int outputMatrix[matrix->cols*matrix->cols];
 		// Vytvoření vstupních matic
 		T_MATRIX inputA, inputB;
@@ -154,32 +159,40 @@ int main(int argc, char *argv[])
 			exit(EXIT_FAILURE);
 		}
 		
+//		cerr<<"Počet procesorů: "<<matrix->processCount<<endl;
+		
 		for(int send = 1; send < matrix->processCount; send++)
 		{
 			// Posílání prvnímu řádku mrížky
 			if(send <= matrix->cols){
 				for(int i = 0; i < tmpC; i++)
 				{
-					MPI_Send(inputB[i][(send-1)], BUFF_SIZE, MPI_INT, send, TAG, MPI_COMM_WORLD);
+					valB = inputB.matrix[i][(send-1)];
+//					cerr<<"valB: "<<valB<<endl;
+					MPI_Send(&valB, BUFF_SIZE, MPI_INT, send, TAG_B, MPI_COMM_WORLD);
 				}
 			}
 			// Posílání prvnímu sloupci mrížky
-			else if(send % matrix->cols == 1){
+			if(send % matrix->cols == 1){
 				for(int i = 0; i < tmpC; i++)
 				{
-					MPI_Send(inputA[(send-1)][i], BUFF_SIZE, MPI_INT, send, TAG, MPI_COMM_WORLD);
+					valA = inputA.matrix[(send/matrix->cols)][i];
+//					cerr<<"valA: "<<valA<<endl;
+					MPI_Send(&valA, BUFF_SIZE, MPI_INT, send, TAG_A, MPI_COMM_WORLD);
 				}
 			}			
 		}
 		
+		cerr<<"odesláno"<<endl;
+		
 		for(int recv = 1; recv < matrix->processCount;recv++)
 		{
-			MPI_Recv(tmpC, BUFF_SIZE, MPI_INT, recv, TAG, MPI_COMM_WORLD, &stat);
+			MPI_Recv(&tmpC, BUFF_SIZE, MPI_INT, recv, TAG, MPI_COMM_WORLD, &stat);
 			outputMatrix[recv-1] = tmpC; 
 		}
 		
 		// Výpis matice
-		for(tmpR = 0; tmp < matrix->processCount-1; tmpR++)
+		for(tmpR = 0; tmpR < matrix->processCount-1; tmpR++)
 		{
 			cout<<outputMatrix[tmpR];
 			if((tmpR+1)%matrix->cols == 0)
@@ -194,13 +207,72 @@ int main(int argc, char *argv[])
 		
 		
 		
-		printMatrix(inputA,matrix->rows,tmpC);
+//		printMatrix(inputA,matrix->rows,tmpC);
 		
-		disposeMatrix(&inputA,matrix->rows,tmpC);
-		disposeMatrix(&inputB,matrix->rows,tmpC);
+//		disposeMatrix(&inputA,matrix->rows,tmpC);
+//		disposeMatrix(&inputB,matrix->rows,tmpC);
 	}
 	else{
+		int valA;
+		int valB;
 //		cout<<"Není to první procesor"<<endl;
+		// Posílání prvnímu řádku mrížky
+		if(matrix->procID <= matrix->cols){
+			for(int i = 0; i < matrix->matN; i++)
+			{
+				MPI_Recv(&valB, BUFF_SIZE, MPI_INT, 0, TAG_B, MPI_COMM_WORLD, &stat);
+				MPI_Recv(&valA, BUFF_SIZE, MPI_INT, matrix->procID-1, TAG_A, MPI_COMM_WORLD, &stat);
+				
+				matrix->C += valA * valB;
+//				cerr<<"IDerr: "<<matrix->procID<<endl;
+				if((matrix->procID+1) % matrix->cols != 1){
+//					cerr<<"Pc: "<<matrix->procID<<" sendTo: "<<matrix->procID+1<<endl;
+					MPI_Send(&valA, BUFF_SIZE, MPI_INT, matrix->procID+1, TAG_A, MPI_COMM_WORLD);
+				}
+				MPI_Send(&valB, BUFF_SIZE, MPI_INT, matrix->procID+matrix->cols, TAG_B, MPI_COMM_WORLD);
+//				cerr<<"IDok: "<<matrix->procID<<endl;
+			}
+			MPI_Send(&matrix->C, BUFF_SIZE, MPI_INT, 0, TAG, MPI_COMM_WORLD);
+		}
+		// Posílání prvnímu sloupci mrížky
+		else if(matrix->procID % matrix->cols == 1 && matrix->procID != 1){
+			for(int i = 0; i < matrix->matN; i++)
+			{
+				MPI_Recv(&valB, BUFF_SIZE, MPI_INT, matrix->procID-matrix->cols, TAG_B, MPI_COMM_WORLD, &stat);
+				MPI_Recv(&valA, BUFF_SIZE, MPI_INT, 0, TAG_A, MPI_COMM_WORLD, &stat);
+				
+				matrix->C += valA * valB;
+//				cerr<<"IDerr: "<<matrix->procID<<endl;
+				MPI_Send(&valA, BUFF_SIZE, MPI_INT, matrix->procID+1, TAG_A, MPI_COMM_WORLD);
+				if(matrix->procID + matrix->cols < matrix->processCount){
+//					cerr<<"Pr: "<<matrix->procID<<" sendTo: "<<matrix->procID+matrix->cols<<endl;
+					MPI_Send(&valB, BUFF_SIZE, MPI_INT, matrix->procID+matrix->cols, TAG_B, MPI_COMM_WORLD);
+				}
+//				cerr<<"IDok: "<<matrix->procID<<endl;
+			}
+			MPI_Send(&matrix->C, BUFF_SIZE, MPI_INT, 0, TAG, MPI_COMM_WORLD);
+		}
+		else
+		{
+			for(int i = 0; i < matrix->matN; i++)
+			{
+				MPI_Recv(&valB, BUFF_SIZE, MPI_INT, matrix->procID-matrix->cols, TAG_B, MPI_COMM_WORLD, &stat);
+				MPI_Recv(&valA, BUFF_SIZE, MPI_INT, matrix->procID-1, TAG_A, MPI_COMM_WORLD, &stat);
+				
+				matrix->C += valA * valB;
+//				cerr<<"IDerr: "<<matrix->procID<<" + test: "<<matrix->procID % matrix->cols<<endl;
+				if((matrix->procID+1) % matrix->cols != 1){
+//					cerr<<"P: "<<matrix->procID<<" sendTo: "<<matrix->procID+1<<endl;
+					MPI_Send(&valA, BUFF_SIZE, MPI_INT, matrix->procID+1, TAG_A, MPI_COMM_WORLD);
+				}
+				if(matrix->procID + matrix->cols < matrix->processCount){
+//					cerr<<"P: "<<matrix->procID<<" sendTo: "<<matrix->procID+matrix->cols<<endl;
+					MPI_Send(&valB, BUFF_SIZE, MPI_INT, matrix->procID+matrix->cols, TAG_B, MPI_COMM_WORLD);
+				}
+//				cerr<<"IDok: "<<matrix->procID<<endl;
+			}
+			MPI_Send(&matrix->C, BUFF_SIZE, MPI_INT, 0, TAG, MPI_COMM_WORLD);			
+		}
 	}
    
 	
--- test.sh
+++ test.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
- 
+
+m1=mat1;
+m2=mat2;
+	
+# Ověření existence prvního souboru
+if [ ! -f $m1 ]; then
+    echo "Soubor mat1 neexistuje!"
+	exit
+fi
+
+# Ověření existence druhého souboru
+if [ ! -f $m2 ]; then
+    echo "Soubor mat2 neexistuje!"
+	exit
+fi
+
 mat1=$(head -n1 mat1)
 mat2=$(head -n1 mat2)
+	
+mat1r=$(cat $m1 | wc -l)
+mat1r=$((mat1r-1))
+mat2r=$(cat $m2 | wc -l)
+mat2r=$((mat2r-1))
+	
+if [ $mat1r -ne $mat1 ]; then
+	n=$mat1r;
+else
+	n=$mat2r;
+fi;
  
 cpus=$((mat1*mat2))
-echo $cpus
- 
+
 mpic++ --prefix /usr/local/share/OpenMPI -o mm mm.cpp -std=c++0x
-mpirun --prefix /usr/local/share/OpenMPI -np $(($cpus+1)) mm
+mpirun --prefix /usr/local/share/OpenMPI -np $(($cpus+1)) mm $n
 rm -f mm
